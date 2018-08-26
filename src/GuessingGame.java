c4e95cfafaf4acd6bc3f31850e45eec351a00bfe
Lisa Rees <kortirion@gmail.com>
2018-08-26 13:08:40 -0400

    add result variable

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -9,12 +9,13 @@ public class GuessingGame {
 		System.out.println("Welcome to the guessing game!");
 		System.out.println("Choose a number between 1 and 10");
 		int guessedNumber = input.nextInt();
-		
+		String result;
 		if (secretNumber == guessedNumber) {
-			System.out.println("You guessed it!");
+			result = "You won!";
 		}else {
-			System.out.println("Nope. You lose.");
+			result = "You lost!";
 		}
+		System.out.println(result);
 		input.close();
 	}
 
